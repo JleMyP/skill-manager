f125190098d0d51bbea60c5aefd58761d862ae3e
Tim <jlemyp96@gmail.com>
2019-11-21 00:30:48 +0500

    view revisions

--- main/views/folders.py
+++ main/views/folders.py
@@ -2,6 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework_serializer_extensions.views import SerializerExtensionsAPIViewMixin
+from reversion.views import RevisionMixin
 
 from ..models import Folder
 from ..serializers.folder import FolderSerializer
@@ -9,7 +10,8 @@ from ..serializers.folder import FolderSerializer
 __all__ = ['FoldersViewSet']
 
 
-class FoldersViewSet(SerializerExtensionsAPIViewMixin,
+class FoldersViewSet(RevisionMixin,
+                     SerializerExtensionsAPIViewMixin,
                      viewsets.ModelViewSet):
     queryset = Folder.objects.all()
     serializer_class = FolderSerializer
