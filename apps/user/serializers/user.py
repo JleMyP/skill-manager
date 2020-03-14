6320c78f7ea5b2f271ed51ec1a6d3d386aed4d68
Tim <jlemyp96@gmail.com>
2020-03-15 02:14:15 +0500

    relate auth backend to default user

--- apps/user/serializers/user.py
+++ apps/user/serializers/user.py
@@ -1,23 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 from rest_framework_serializer_extensions.serializers import SerializerExtensionsMixin
 
-from ..models import CustomUser
-
 __all__ = ['RegistrationSerializer', 'ProfileSerializer']
+
+user_model = get_user_model()
 
 
 class RegistrationSerializer(serializers.ModelSerializer):
     """Кастомный юзер и хз че еще."""
 
-    _validator = UniqueValidator(queryset=CustomUser.objects.all())
+    _validator = UniqueValidator(queryset=user_model.objects.all())
     email = serializers.EmailField(validators=[_validator], required=False, write_only=True)
     username = serializers.CharField(validators=[_validator], write_only=True)
 
     class Meta:
-        model = CustomUser
+        model = user_model
         fields = ('username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
@@ -28,7 +29,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
 class ProfileSerializer(SerializerExtensionsMixin, serializers.ModelSerializer):
     class Meta:
-        model = CustomUser
+        model = user_model
         fields = ('username', 'email', 'first_name', 'last_name', 'middle_name',
                   'is_staff', 'date_joined')
         read_only_fields = ('username', 'email', 'is_staff', 'date_joined')
